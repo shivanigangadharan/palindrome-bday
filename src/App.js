90fd89ff0b821f6bc8958e71b7c309ddac51b0f6
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-09-04 23:53:15 +0530

    feat: skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState();
+
+  const [ddmmyyyy, setDdmmyyyy] = useState();
+  const [mmddyyyy, setMmddyyyy] = useState();
+  const [yyyymmdd, setYyyymmdd] = useState();
+  const [ddmmyy, setDdmmyy] = useState();
+  const [mmddyy, setMmddyy] = useState();
+  const [yymmdd, setYymmdd] = useState();
+  const [palindrome, setPalindrome] = useState();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setDdmmyyyy(
+      date.slice(8, 10) + date.slice(5, 7) + date.slice(0, 4) ===
+        (date.slice(8, 10) + date.slice(5, 7) + date.slice(0, 4)).split('').reverse().join('') ? 'Pal' : 'Not'
+    );
+
+    setMmddyyyy(
+      date.slice(5, 7) + date.slice(8, 10) + date.slice(0, 4) ===
+        (date.slice(5, 7) + date.slice(8, 10) + date.slice(0, 4)).split('').reverse().join('') ? 'Pal' : 'Not'
+    );
+    setYyyymmdd(
+      date.slice(0, 4) + date.slice(5, 7), date.slice(8, 10) ===
+        (date.slice(0, 4) + date.slice(5, 7), date.slice(8, 10)).split('').reverse().join('') ? 'Pal' : 'Not'
+    );
+
+    setDdmmyy(
+      date.slice(8, 10) + date.slice(5, 7) + date.slice(2, 4) ===
+        (date.slice(8, 10) + date.slice(5, 7) + date.slice(2, 4)).split('').reverse().join('') ? 'Pal' : 'Not'
+    );
+    setMmddyy(
+      date.slice(5, 7) + date.slice(8, 10) + date.slice(2, 4) ===
+        (date.slice(5, 7) + date.slice(8, 10) + date.slice(2, 4)).split('').reverse().join('') ? 'Pal' : 'Not'
+    );
+
+    setYymmdd(
+      date.slice(2, 4) + date.slice(5, 7), date.slice(8, 10) ===
+        (date.slice(2, 4) + date.slice(5, 7), date.slice(8, 10)).split('').reverse().join('') ? 'Pal' : 'Not'
+    );
+
+    if (ddmmyyyy === 'Pal' || mmddyyyy === 'Pal' || yyyymmdd === 'Pal' || ddmmyy === 'Pal' || mmddyy === 'Pal' || yymmdd === 'Pal') {
+      setPalindrome('Palindrome');
+    }
+    else {
+      setPalindrome('Not a palindrome');
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h1>Palindrome Birthday </h1>
+        <form>
+          <label> Enter your birthday </label><br />
+          <input required type="date" onChange={(e) => setDate(e.target.value)} /> <br />
+          <button onClick={handleSubmit}> Show </button>
+        </form>
+        <div hidden={palindrome === undefined ? true : false}>
+          {
+            palindrome
+          }
+        </div>
+      </center>
     </div>
   );
 }
